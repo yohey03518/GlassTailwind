a477313b875c9ffa99d8072d873f19a64440c55b
erwin <erwin.chang@titansoft.com.sg>
2024-07-24 17:48:20 +0800

    complete header logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./src/**/*.{html,js}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{html,js}"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'red': '#AA0601'
+            }
+        },
+    },
+    plugins: [],
 }
 
